830e3c667fb178cb3fc7f8a2a65b25f94c4a984f
kenjones <kenjones@cisco.com>
2017-04-21 09:52:04 -0400

    Remove source plugin descriptor
    The use of `source` pollutes the global namespace with extra variables.
    By capturing the only value required from the file and passing it
    explicitly it will avoid the "magic" of where the value of the variable
    originated.

--- babun-core/tools/plugins.sh
+++ babun-core/tools/plugins.sh
@@ -6,27 +6,28 @@ source "$babun_tools/script.sh"
 
 function plugin_should_install {
     local plugin_name="$1"
-    local __result=$2
+    local plugin_version="$2"
+    local __resultvar=$3
     local installed="$babun/installed/$plugin_name"
     if [[ -f "$installed" ]]; then
         typeset -i installed_version
         local installed_version
         installed_version=$(cat "$installed" || echo "0")
 
-        # shellcheck disable=SC2154
         if ! [[ $plugin_version -gt $installed_version ]]; then
             echo "  installed [$installed_version]"
             echo "  newest    [$plugin_version]"
             echo "  action    [skip]"
-            eval "$__result=0"
+            eval "$__resultvar=0"
             return 0
         fi
     fi
-    eval "$__result=1"
+    eval "$__resultvar=1"
 }
 
 function plugin_installed_ok {
     local plugin_name="$1"
+    local plugin_version="$2"
     local installed="$babun/installed/$plugin_name"
     if [[ -f "$installed" ]]; then
         typeset -i installed_version
@@ -53,13 +54,12 @@ function plugin_install {
         exit 1
     fi
 
-    # loads the plugin descriptor
-    # shellcheck disable=SC1090
-    source "$plugin_desc"
+    local plugin_version
+    plugin_version=$(awk -F '=' '/plugin_version/ { print $2}' "$plugin_desc")
 
     # checks the version, install only if the version is newer
     # uses the plugin descriptor variables
-    plugin_should_install "$plugin_name" result
+    plugin_should_install "$plugin_name" "$plugin_version" result
     # shellcheck disable=SC2154
     if [[ "$result" -eq "0" ]]; then
         return 0
@@ -73,7 +73,7 @@ function plugin_install {
 
     # sets the version to the newest one
     # uses the plugin descriptor variables
-    plugin_installed_ok "$plugin_name"
+    plugin_installed_ok "$plugin_name" "$plugin_version"
 }
 
 function plugin_install_home {
@@ -84,10 +84,6 @@ function plugin_install_home {
         echo "ERROR: Cannot find plugin descriptor [$plugin_name] [$plugin_desc]"
         exit 1
     fi
-
-    # loads the plugin descriptor
-    # shellcheck disable=SC1090
-    source "$plugin_desc"
 
     # execute plugin's install_home.sh in a separate shell
     local install_home_script="$babun/source/babun-core/plugins/$plugin_name/install_home.sh"
