56980375e76eba9b33e7807a54a9f1ae8a655af0
flythink <zjx632@gmail.com>
2018-08-19 12:01:34 +0800

    fix build script

--- babun-core/plugins/cacert/install.sh
+++ babun-core/plugins/cacert/install.sh
@@ -6,10 +6,10 @@ source "$babun_tools/script.sh"
 
 run() {
     # reference http://stackoverflow.com/a/4454754
-    #pushd /usr/ssl/certs 2>/dev/null
+    pushd /usr/ssl/certs 2>/dev/null
     #curl https://curl.haxx.se/ca/cacert.pem | awk '{print > "cert" (1+n) ".pem"} /-----END CERTIFICATE-----/ {n++}'
     #c_rehash
-    #popd 2>/dev/null
+    popd 2>/dev/null
 }
 
 run
